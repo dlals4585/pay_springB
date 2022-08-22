3c964946969c44b293c05b5e6477a569529900fb
dlals4585 <dlals4585@naver.com>
2022-08-23 06:20:17 +0900

    lombok + dependencies 정리 + TestCode작성 + VO적용

---
+++ src/main/java/com/trip/letscamping/web/vo/MainResponseVO.java
@@ -0,0 +1,12 @@
+package com.trip.letscamping.web.vo;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class MainResponseVO {
+    private final String name;
+    private final int amount;
+
+}
---
+++ src/test/java/com/trip/letscamping/web/vo/MainResponseVOTest.java
@@ -0,0 +1,22 @@
+package com.trip.letscamping.web.vo;
+
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MainResponseVOTest {
+    @Test
+    public void 롬북_기능_테스트(){
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        MainResponseVO vo = new MainResponseVO(name, amount);
+
+        //then
+        assertThat(vo.getName()).isEqualTo(name);
+        assertThat(vo.getAmount()).isEqualTo(amount);
+    }
+}
